8b27e4889859732c4bd05156dbbbcb778e54b8d5
swiftninjav2@gmail.com <swiftninjav2@gmail.com@531c8527-694a-c03c-2849-e0159db3aee4>
2015-02-16 14:24:19 +0000

    Can now create list of most messaged people.

--- sms_fun/savenumbers.pm
+++ sms_fun/savenumbers.pm
@@ -13,8 +13,32 @@ my $parser = XML::LibXML::Simple->new;
 
 #read XML File
 
-my $xml=$parser->XMLin("archive.xml");
+my $xml=$parser->XMLin("archive.xml",
+    ForceArray=> 1,
+#    KeyAttr => "date",
+);
+my %call_count = ();
 my @list = @{$xml->{sms}};
     for my $sms (@list){
-        print $sms->{contact_name}."=>".$sms->{body}."\n";
+#        print $sms->{contact_name}."=>".$sms->{address}."\n";
+#        foreach {
+#            $call_count{$sms->{contact_name}}++;
+#        }
+        if ( exists($call_count{$sms->{contact_name}}) ){
+            $call_count{$sms->{contact_name}}++;
+#            print "true";
+        }else{
+            $call_count{$sms->{contact_name}} = '1';
+#            print "false \n";
+        }
+#        print Dumper(%call_count);
     }
+#foreach my $name (keys %call_count) {
+#            print "$name: $call_count{$name}\n";
+#        };
+
+foreach my $text_count (sort {$call_count{$b} <=> $call_count{$a}} keys %call_count) {
+    print "$text_count: $call_count{$text_count}\n";
+};
+    
+    
