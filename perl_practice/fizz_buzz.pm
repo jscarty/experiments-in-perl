5b71a2944accce167622b8a4f24c5bdf69f49dfd
swiftninjav2@gmail.com <swiftninjav2@gmail.com@531c8527-694a-c03c-2849-e0159db3aee4>
2015-03-09 22:04:51 +0000

    so I can look at it at work

---
+++ perl_practice/fizz_buzz.pm
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Data::Dumper;
+
+
+my $total;
+my $fizzpoint;
+sub fizzbuzz{
+    $fizzpoint = shift;
+    for (my $i = 0; $i <= $fizzpoint) {
+        if ($i/3 && /\\D/){
+        
+        }
+    }
+}
