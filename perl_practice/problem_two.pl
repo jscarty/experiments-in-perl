a52e878d699b4347d330c04e523871043f61ed8e
Jason Carty <Jcarty>
2015-03-24 22:08:53 +0000

    Solution to problem 2
    It’s the solution but brute forced.

--- perl_practice/problem_two.pl
+++ perl_practice/problem_two.pl
@@ -5,18 +5,19 @@ use feature 'say';
 
 my $number;
 my $answer;
-my @fib_numbers = ("1","2");
+my @fib_numbers = ("2");
+my @counter = ("1","2");
 
-
-    while ($fib_numbers[-1]< 4_000_000){
-        my $even_number = $fib_numbers[-2] + $fib_numbers[-1];
+    while ($counter[-1]< 4_000_000){
+        push (@counter, $counter[-2] + $counter[-1]);
+        my $even_number = $counter[-2] + $counter[-1];
         if ($even_number % 2 == 0){
             push (@fib_numbers, $even_number);
         }
     }
-#   if ($fib_numbers[-1]>4_000_000){
-#    pop(@fib_numbers);
-#   }
+  if ($fib_numbers[-1]>4_000_000){
+   pop(@fib_numbers);
+  }
     $answer += $_ for @fib_numbers;
 say $answer;
 say "2%3";
